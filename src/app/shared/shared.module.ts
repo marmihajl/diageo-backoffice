ddcbe61355495916a546756f957b9bcfcbc8b374
Kristijan Milanovic <kristijan.milanovic@hivetech.hr>
2022-02-23 14:57:11 +0100

    refactor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MainNavComponent } from "./main-nav/main-nav.component";
 import { MaterialModule } from "../core/material/material.module";
 import { RouterModule } from "@angular/router";
-import { HttpTableComponent } from './http-table/http-table.component';
 import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
   declarations: [
     MainNavComponent,
-    HttpTableComponent,
   ],
   imports: [
     CommonModule,
@@ -20,7 +18,6 @@ import { HttpClientModule } from "@angular/common/http";
   ],
   exports: [
     MainNavComponent,
-    HttpTableComponent,
     HttpClientModule
   ]
 })
